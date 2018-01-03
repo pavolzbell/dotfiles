1fc8a6324417f6ccd727b8a17ff1cf7f339e4a12
Pavol Zbell <pavol.zbell@gmail.com>
2018-01-03 11:44:00 +0100

    Fix fish_colors function

--- .config/fish/functions/fish_colors.fish
+++ .config/fish/functions/fish_colors.fish
@@ -1,6 +1,6 @@
 function fish_colors
-  for color in (set | grep "^fish_color_" | cut -f 1 -d " ");
-    set_color "$$color"; echo -n "$color $$color"
-    set_color -b normal; echo
+  for color in (set | grep "^fish_\(pager_\)\?color_" | cut -f 1 -d " ");
+    set_color $$color; echo -n "$color $$color"
+    set_color normal; echo
   end
 end
