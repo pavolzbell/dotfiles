bbb0707542ffb40442dac92e0b8836ef4a06f813
Pavol Zbell <pavol.zbell@gmail.com>
2018-08-26 19:50:39 +0200

    Simpler bootstrap

--- bootstrap.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cd "$(dirname "${BASH_SOURCE}")"
-
-git pull origin master
-
-rsync -avh --no-perms --exclude ".git" --exclude-from ".gitignore" --exclude "bootstrap.sh" --exclude "*.md" . ~
